7c2ca416aa0fd974d4b1d325ca8a2d5b757443f6
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2024-01-14 20:21:25 +0200

    add: user profile Route + auth

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,7 +6,7 @@ import {
   getUserById,
   getUserProfile,
 } from "../controllers/userController.js";
-
+import authMiddleware from "./auth.routes.js";
 const router = express.Router();
 
 // GET all users
@@ -20,5 +20,8 @@ router.post("/", userSignUp);
 
 // POST - User Login
 router.post("/login", userLogin);
+
+//GET - User Profile
+router.get("/profile/:id", authMiddleware, getUserProfile);
 
 export default router;
