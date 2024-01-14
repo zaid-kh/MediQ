93a0d958af47f08df7f9f150cc198850483e3001
jameel Agbaria <agbariajameel@gmail.com>
2024-01-14 14:53:34 +0200

    login and register and installed react router dom added routes to app jsx with mui theme and css baseline

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import "./App.css";
+import Login from "./pages/Login/login";
+import { lightBlue } from "@mui/material/colors";
+import Register from "./pages/Register/Register";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>MediQ</h1>
-    </>
-  );
+    const theme = createTheme({
+        palette: {
+            primary: lightBlue,
+        },
+    });
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default App;
