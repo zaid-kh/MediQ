3e225c5b4d2e955f7c9123621c8214c7a6e983f2
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-01-14 21:28:51 +0000

    add the specialties files and icons

---
+++ client/public/data/specialties.js
@@ -0,0 +1,45 @@
+export const specialties = [
+    {
+      id: 1,
+      specialtyName: "Mental Health",
+      icon: 'mentalHealth', 
+      color: '#3498db', 
+      background: '#ecf0f1', 
+    },
+    {
+      id: 2,
+      specialtyName: "Cardiovascular Health",
+      icon: 'heartRateMonitor', 
+      color: '#e74c3c', 
+      background: '#f2dede', 
+    },
+    {
+      id: 3,
+      specialtyName: "Orthopedic Health",
+      icon: 'skeleton', 
+      color: '#2ecc71', 
+      background: '#dff0d8', 
+    },
+    {
+      id: 4,
+      specialtyName: "Dermatological Health",
+      icon: 'smileFace', 
+      color: '#f39c12', 
+      background: '#fcf8e3', 
+    },
+    {
+      id: 5,
+      specialtyName: "Ophthalmic Health",
+      icon: 'eye', 
+      color: '#8e44ad', 
+      background: '#e8daef', 
+    },
+    {
+      id: 6,
+      specialtyName: "Pediatric Health",
+      icon: 'baby', 
+      color: '#e67e22', 
+      background: '#fdebd0', 
+    },
+  ];
+  
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,35 +1,82 @@
-import React from 'react'
-import WelcomeBox from '../../components/Cards/WelcomeBox'
-import NavBar from '../../components/NavBar/NavBar'
-import SpecialtyCard from '../../components/Cards/SpecialtyCard'
+import React, { useEffect, useState } from 'react';
+import WelcomeBox from '../../components/Cards/WelcomeBox';
+import NavBar from '../../components/NavBar/NavBar';
+import SpecialtyCard from '../../components/Cards/SpecialtyCard';
+import Header from '../../components/Header/Header';
+import axios from 'axios';
+import { useTheme } from '@mui/material/styles'; 
+import { useAuth } from '../../context/AuthContext';
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { specialties } from '../../../public/data/specialties';
 
-import Header from '../../components/Header/Header'
 function Dashboard() {
+  const { accessToken, logout } = useAuth();
+  const [userData, setUserData] = useState(null);
+  const theme = useTheme();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        if (accessToken) {
+          const response = await axios.get('https://mediq-service.onrender.com/api/v1/user', {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          setUserData(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        logout(); 
+      }
+    };
+
+    fetchUserData();
+  }, [accessToken, logout]);
+
   return (
     <>
-    <div style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
-        gap: "15px",
-        padding:"0px 42px 22px 43px",
-    }}>
-        
-        <Header 
-          profileImg = {""}
-          name= {""}
+       <ThemeProvider theme={theme}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+          gap: '15px',
+          padding: '0px 42px 22px 43px',
+          backgroundColor: theme.palette.background.default, 
+        }}
+      >
+        <Header
+          profileImg={userData?.profileImg || 'https://tse4.mm.bing.net/th?id=OIP.wEsBe2udHBieFeZVmus8qAHaHk&pid=Api&P=0&h=180'}
+          name={userData?.name || 'Guest'}
         />
         <WelcomeBox />
-
-        <SpecialtyCard />
-    
-    </div>
-    <footer>
+        <div
+        style={{
+          display:"flex",
+          flexWrap:"wrap",
+          justifyContent:"space-between"
+          
+        }}>
+        {specialties.map((specialty)=> (
+          <SpecialtyCard
+          key={specialty.id}
+          icon = {specialty.icon}
+          color = {specialty.color}
+          backgroundColor= {specialty.background} specialtyName = {specialty.specialtyName}
+          />       
+          )) 
+        }
+        </div>
+      </div>
+      <footer>
         <NavBar />
-    </footer>
+      </footer>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
