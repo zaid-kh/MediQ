6733cf723cd02bd8fc42ec16e524c2302a0fed21
jameel Agbaria <agbariajameel@gmail.com>
2024-01-14 15:48:36 +0200

    added welcome page

--- client/src/pages/Login/login.jsx
+++ client/src/pages/Login/login.jsx
@@ -9,6 +9,7 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { Slide } from "@mui/material";
 
 export default function Login() {
     const handleSubmit = (event) => {
@@ -21,68 +22,72 @@ export default function Login() {
     };
 
     return (
-        <Container component="main" maxWidth="xs">
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
+        <Slide direction="left" in>
+            <Container component="main" maxWidth="xs">
                 <Box
-                    component="form"
-                    onSubmit={handleSubmit}
-                    noValidate
-                    sx={{ mt: 1 }}
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
                 >
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
+                    <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ mt: 1 }}
                     >
-                        Sign In
-                    </Button>
-                    <Grid container>
-                        <Grid item>
-                            <Link href="/register" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox value="remember" color="primary" />
+                            }
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item>
+                                <Link href="/register" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </Box>
                 </Box>
-            </Box>
-        </Container>
+            </Container>
+        </Slide>
     );
 }
--- client/src/pages/Welcome/Welcome.jsx
+++ client/src/pages/Welcome/Welcome.jsx
@@ -1,19 +1,29 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Slide, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
 export default function Welcome() {
     const navigate = useNavigate();
     return (
-        <div className="Welcome Page">
-            <MedicalInformationIcon
-                sx={{ width: "100px", height: "100px", color: "primary.main" }}
-            />
-            <Typography variant="h4">
-                Welcome To MediQ Your AI Medical Assistant
-            </Typography>
-            <Button fullWidth variant="contained" onClick={() => navigate("/login")}>
-                Get Started
-            </Button>
-        </div>
+        <Slide direction="up" in>
+            <div className="Welcome Page">
+                <MedicalInformationIcon
+                    sx={{
+                        width: "100px",
+                        height: "100px",
+                        color: "primary.main",
+                    }}
+                />
+                <Typography variant="h4">
+                    Welcome To MediQ Your AI Medical Assistant
+                </Typography>
+                <Button
+                    fullWidth
+                    variant="contained"
+                    onClick={() => navigate("/login")}
+                >
+                    Get Started
+                </Button>
+            </div>
+        </Slide>
     );
 }
