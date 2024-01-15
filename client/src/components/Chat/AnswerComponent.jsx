2aefea86131c486853d449b60b02da86616bec5b
ydaher97 <73422885+ydaher97@users.noreply.github.com>
2024-01-15 14:35:41 +0200

    the chat

--- client/src/components/Chat/AnswerComponent.jsx
+++ client/src/components/Chat/AnswerComponent.jsx
@@ -2,7 +2,7 @@ import { lightBlue } from '@mui/material/colors';
 import React from 'react'
 import { FaUserDoctor } from "react-icons/fa6";
 import './Chat.css'
-function AnswerComponent({ answer }) {
+function AnswerComponent( {answer} ) {
   console.log(answer)
   return (
     <div  style={{
@@ -13,7 +13,7 @@ function AnswerComponent({ answer }) {
     }}>
         <FaUserDoctor color="#FAFAFA" fontSize="24px" />
         <div className='answer' style={{ textAlign: 'right', margin: '10px 0', padding: '10px', background: '#f0f0f0', borderRadius: '8px',  maxWidth:'45%',position: "relative"}}>
-            <p style={{ margin: '0' }}>{answer?.content}</p>
+            <p style={{ margin: '0' }}>{answer}</p>
         </div>
   </div>
   )
